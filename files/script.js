8e70fa0e8d818814917a3066cac0e5dff1f17a95
Tommy Hoang <tommyyhoangg@gmail.com>
2020-11-21 13:22:31 +0100

    Added more animations to the home page

--- files/script.js
+++ files/script.js
@@ -24,11 +24,30 @@ benTl.from(".benefits-h1, .title-seperator2, .benefits-p1, .benefits-p2", {opaci
 const wuTl = gsap.timeline({
     scrollTrigger: {
         trigger: ".whyus",
-        start: ".center bottom"
+        start: "center bottom"
     }
 });
 
-wuTl.from(".whyus-h1, .whyus-p1, .whyus-p2", {x: "-50%", opacity: 0, duration: 1, delay: 0.4})
+wuTl.from(".whyus-h1, .whyus-p1, .whyus-p2", {x: "-50%", opacity: 0, duration: 1, delay: 0.1})
     .from(".title-seperator3", {x: "-50%", opacity: 0}, "-=0.5")
-    .from(".bubble-1, .bubble-2, .bubble-3", {opacity: 0, duration: 0.4}, "-=0.4")
-    .from(".whyus-img", {duration: 1, opacity: 0, delay: 0.7});
+    .from(".bubble-1, .bubble-2, .bubble-3", {opacity: 0, duration: 0.3}, "-=0.16")
+    .from(".whyus-img", {duration: 1.2, opacity: 0, delay: 0.75});
+
+const TestTl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".testimonials",
+        start: "center bottom"
+    }
+});
+
+TestTl.from(".test-info h1, .title-seperator6, .test-user, .test-sign, .test-button", {opacity: 0, duration: 3, stagger: 0.05, delay: 0.2});
+
+const ContTl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".contact",
+        start: "center bottom"
+    }
+});
+
+ContTl.from(".contact-info", {x: "-50%", opacity: 0, duration: 1.3})
+ContTl.from(".contact-footer", {x: "50%", opacity: 0, duration: 1.3}, "-=1.3")
